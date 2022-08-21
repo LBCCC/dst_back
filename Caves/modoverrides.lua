bed89c1be08dc8315c6cdf788a16d6d5bd90d0d0
jiaxiao <jiaxiao@pinduoudo.com>
2022-08-21 14:22:23 +0800

    cundang

---
+++ Caves/modoverrides.lua
@@ -0,0 +1,18 @@
+return {
+  ["workshop-458587300"]={ configuration_options={ Ownership=false, Travel_Cost=32 }, enabled=true },
+  ["workshop-666155465"]={
+    configuration_options={
+      chestB=-1,
+      chestG=-1,
+      chestR=-1,
+      display_hp=-1,
+      food_estimation=-1,
+      food_order=0,
+      food_style=0,
+      lang="auto",
+      show_food_units=-1,
+      show_uses=-1 
+    },
+    enabled=true 
+  } 
+}
